903e7851ee95dbbb1c6a4e1ddc4e5a4d6b688b1b
Ahamad Rishad <aahamadrishad@gmail.com>
2024-01-23 15:43:32 -0500

    final project 'first commit'

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
 
+import './App.css';
+import StyledCard from './components/StyledCard';
+import contacts from './data/contacts';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <>
+   
+  <StyledCard
+  name={contacts[0].name}
+  imgURL={contacts[0].imgURL}
+  phone={contacts[0].phone}
+  email={contacts[0].email}
+  />
+
+<StyledCard
+  name={contacts[1].name}
+  imgURL={contacts[1].imgURL}
+  phone={contacts[1].phone}
+  email={contacts[1].email}
+  />
+
+<StyledCard
+  name={contacts[2].name}
+  imgURL={contacts[2].imgURL}
+  phone={contacts[2].phone}
+  email={contacts[2].email}
+  />
+   </>
   );
 }
 
